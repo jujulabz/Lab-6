05ee5787c818d248928d6764a087fd4c1dd3dd4b
jujulabz <mp678503@gmail.com>
2026-02-22 22:32:45 -0500

    Create Lab6_jujulabz-1.py
    Created python file and added comments.

---
+++ Lab6_jujulabz-1.py
@@ -0,0 +1,11 @@
+"""
+
+User Login System,
+Melissa Palmer,
+The program t simulates a user login
+system using a dictionary to store usernames
+and passwords.,
+Any info about starter code (If used, where it came from, link, etc.), and the
+Date.
+
+"""
