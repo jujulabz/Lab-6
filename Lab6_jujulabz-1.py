cd8b327562170e051d083f044f5e6238511e3817
jujulabz <mp678503@gmail.com>
2026-02-22 23:48:50 -0500

    Added comments  to explain the code since it was confusing at first.

--- Lab6_jujulabz-1.py
+++ Lab6_jujulabz-1.py
@@ -9,26 +9,26 @@ Any info about starter code (If used, where it came from, link, etc.), and the
 Date.
 
 """
-
+#dictonary of users and passwords
 user = {'guest': 'guest',
             'Green0':'Red0',
             'Chesse': 'Burger2',
             'mpalmer33': 'login65'
             }
 
-username = input(f'Enter Username: ')
+username = input(f'Enter Username: ') #user input im asking for the users username
 
 if username not in user:
-    print('User not found')
+    print('User not found') #if the user doesnt match anything inside my dictonary then the program tells users that the user can not be found
 else:
-    password = input(f'Enter Password: ')
+    password = input(f'Enter Password: ') # now we go to the second condition if username is user then users get prompted to enter a password
     
-    if password != user[username]:
+    if password != user[username]: # if the password does not match the associated password with the username then access is denied
         print('\nAccess Denied.')
     
     else:
-        if username == "guest":
-            print(f"\nWelcome, {username}. You have Guest access.")
+        if username == "guest": #here the program check if the user is the guest account 
+            print(f"\nWelcome, {username}. You have Guest access.") # if true then prints out this welcome message this works for all other users in my dictonary
         else:
-            print(f"\nWelcome, {username}. You have Security Level 1.")
+            print(f"\nWelcome, {username}. You have Security Level 1.") # prints out security level if all bools reult in true 
        
